7f80acdbf806ea80bc8feee186a9a49f3641f891
carrynong2 <carrynong2@gmail.com>
2023-12-27 19:25:00 +0700

    style checkoutview

--- src/views/user/CartView.vue
+++ src/views/user/CartView.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { RouterLink } from "vue-router";
+
 import UserLayout from "@/layouts/UserLayout.vue";
 import Close from "@/components/icons/Close.vue";
 import { useCartStore } from "@/stores/user/cart";
@@ -70,6 +72,11 @@ const changeQuantity = (event, index) => {
             <div>ราคารวมท้ังหมด</div>
             <div>{{ cartStore.summaryPrice }}</div>
           </div>
+          <RouterLink
+            :to="{ name: 'checkout' }"
+            class="btn btn-neutral w-full mt-4"
+            >ชำระเงิน</RouterLink
+          >
         </div>
       </div>
     </div>
--- src/views/user/CheckoutView.vue
+++ src/views/user/CheckoutView.vue
@@ -1,7 +1,97 @@
 <script setup>
 import UserLayout from "@/layouts/UserLayout.vue";
+import { reactive } from "vue";
+import { RouterLink } from "vue-router";
+
+import { useCartStore } from "@/stores/user/cart.js";
+
+const cartStore = useCartStore();
+const formData = [
+  {
+    name: "Email address",
+    field: "email",
+  },
+  {
+    name: "Name",
+    field: "name",
+  },
+  {
+    name: "Address",
+    field: "address",
+  },
+  {
+    name: "Note",
+    field: "note",
+  },
+];
+
+const userFormData = reactive({
+  email: "",
+  name: "",
+  address: "",
+  note: "",
+});
+
+const payment = () => {
+  console.log(userFormData);
+};
 </script>
 
 <template>
-  <UserLayout> CheckOut Page </UserLayout>
+  <UserLayout>
+    <h1 class="text-3xl font-bold m-4">Checkout</h1>
+    <div class="flex">
+      <section class="flex-auto w-64 bg-base-200 p-8">
+        <div v-for="form in formData" class="form-control w-full">
+          <label class="label">
+            <span class="label-text">{{ form.name }}</span>
+          </label>
+          <input
+            v-model="userFormData[form.field]"
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
+          />
+        </div>
+        <button @click="payment" class="btn btn-neutral w-full mt-4">
+          ชำระเงิน
+        </button>
+      </section>
+      <section class="flex-auto w-32 bg-slate-200 px-2">
+        <div v-for="item in cartStore.items" class="flex bg-white m-8 py-4">
+          <div class="flex-1">
+            <img class="w-full p-8" :src="item.imageUrl" />
+          </div>
+          <div class="flex-1">
+            <div class="flex flex-col justify-between h-full">
+              <div>
+                <div>
+                  <b>{{ item.name }}</b>
+                </div>
+                <div>จำนวน {{ item.quantity }}</div>
+              </div>
+              <RouterLink :to="{ name: 'cart' }">Edit</RouterLink>
+            </div>
+          </div>
+        </div>
+        <div class="divider"></div>
+        <div class="p-4">
+          <div><b>Order Summary</b></div>
+          <div class="flex justify-between">
+            <div>ราคาสินค้าทั้งหมด</div>
+            <div>{{ cartStore.summaryPrice }}</div>
+          </div>
+          <div class="flex justify-between">
+            <div>ค่าส่ง</div>
+            <div>0</div>
+          </div>
+        </div>
+        <div class="divider"></div>
+        <div class="flex justify-between p-4 mb-4">
+          <div>ราคาทั้งหมด</div>
+          <div>{{ cartStore.summaryPrice }}</div>
+        </div>
+      </section>
+    </div>
+  </UserLayout>
 </template>
